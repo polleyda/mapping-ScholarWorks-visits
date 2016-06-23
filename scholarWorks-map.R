388775c785acba8a57a306cdafb7789eed20c811
dapolley <dapolley@iupui.edu>
2016-06-23 09:53:20 -0400

    update to R script

--- scholarWorks-map.R
+++ scholarWorks-map.R
@@ -3,19 +3,22 @@ x <- c("ggplot2", "rgdal", "scales", "ggmap","plyr", "dplyr", "maptools", "ggalt
 lapply(x, require, character.only = TRUE)
 
 # Set working directory to Desktop
-setwd("C:/Users/dapolley/Desktop")
+setwd("C:/Users/dapolley/Desktop/ScholarWorks-maps")
 
 # Read in the data
-data <- read.csv("visits_by_country.csv", header = TRUE)
+data <- read.csv("map_data_template.csv", header = TRUE, na.strings = "NA")
 
 # Read in world map w/ country boundaries shapefile
-map <- readOGR(dsn = "TM_WORLD_BORDERS-0.3", layer = "TM_WORLD_BORDERS-0.3")
+map <- readOGR(dsn = "mapping-ScholarWorks-visits/TM_WORLD_BORDERS-0.3", layer = "TM_WORLD_BORDERS-0.3")
 
 # Remove Antarctica from shapefile
-map <- subset(map, map$ISO2 != "AQ")
+map <- subset(map, map$ISO3 != "ATA")
 
 # Coerce map into dataframe ggplot2 can recognize
 map <- fortify(map, region = "ISO3")
+
+# Rename is03 column to id
+colnames(data) <- c("sort_id", "common_name", "formal_name", "id", "visits")
 
 # Convert data$id from factor to character to ease merge
 data[,"id"] <- as.character(data[,"id"])
@@ -25,7 +28,7 @@ plot_data <- left_join(map, data)
 
 # Map data
 ggplot() +
-  geom_polygon(data = plot_data, aes(x = long, y = lat, group = group, fill = site_visits), color = "black", size = 0.05) +
+  geom_polygon(data = plot_data, aes(x = long, y = lat, group = group, fill = visits), color = "black", size = 0.05) +
   coord_proj("+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs") +
   scale_fill_gradient(low = "#fee8c8", high = "#e34a33", trans = "sqrt") +
   theme(axis.ticks = element_blank(),
